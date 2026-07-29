654c333d588bc0702782fac3093104f8ace0c472
aagilsakti16-beep <aagilsakti16@gmail.com>
2026-07-29 18:57:50 +0700

    Update planet descriptions in script.js

--- script.js
+++ script.js
@@ -587,10 +587,10 @@ scene.add(planet);
 
 //Ganti Deskripsi Planet
 const ringTexts = [
-  'Galaxy of love From Agilang Sakti...',//untuk deskripsi planet layer 1
-  "I love you Nayla Alzafira",//untuk deskripsi planet layer 2
+  'Galaxy of love From Agilang',//untuk deskripsi planet layer 1
+  "I love you Thania",//untuk deskripsi planet layer 2
   "♡You Are My Favorite Person♡",//untuk deskripsi planet layer 3
-  "01/02/2026",//untuk deskripsi planet layer 4
+  "01/08/2026",//untuk deskripsi planet layer 4
   ...(window.dataCCD && window.dataCCD.data.ringTexts ? window.dataCCD.data.ringTexts : [])
 ];
 
